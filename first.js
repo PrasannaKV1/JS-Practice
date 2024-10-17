62fa1d4e5890775fe0ffa93143e114c5dc1c1de2
PrasannaKV1 <prasannapooja215@gmail.com>
2024-10-17 17:39:22 +0530

    This is simply changes are made by perasannakv

--- first.js
+++ first.js
@@ -4,3 +4,7 @@ console.error("there is bug");
 console.log(new Date().toLocaleDateString());
 console.log(new Date().toLocaleTimeString());
 console.log("Hello Prasanna");
+for(let i=0;i<10;i++)
+{
+   console.log(i);
+}
