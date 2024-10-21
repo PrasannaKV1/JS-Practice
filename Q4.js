62af7ea5e7b1a996046491fce7130a40d271a48e
PrasannaKV1 <prasannapooja215@gmail.com>
2024-10-21 22:27:23 +0530

    im pushig my fourth code

---
+++ Q4.js
@@ -0,0 +1,9 @@
+let forchild=100;
+let foradult=150;
+let forseniors=200;
+//number of people 
+let nchild=2;
+let nadult=2;
+let nseniors=3;
+const totalprice =(forchild*nchild)+(foradult*nadult)+(forseniors*nseniors);
+console.log(totalprice);
