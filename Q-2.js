3a6dacfd22162698069618b289a575ca73ddfdb1
PrasannaKV1 <prasannapooja215@gmail.com>
2024-10-21 22:11:48 +0530

    I'm Prasanna K V

---
+++ Q-2.js
@@ -0,0 +1,9 @@
+let username='Prasanna K V';
+let passowrd="prasanna@528";
+if(username==="Prasanna KV"&&passowrd==="prasanna@528")
+{
+    console.log("Logged in successfully");
+}
+else{
+    console.log("invalid credentials");
+}
