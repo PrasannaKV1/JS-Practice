abafb19f468c9fd854728ef37a49409b34efda44
PrasannaKV1 <prasannapooja215@gmail.com>
2024-10-26 08:20:41 +0530

    Many files changes

---
+++ Eight.js
@@ -0,0 +1,7 @@
+console.log("print the numbers of 10 to 1");
+let num=10;
+while(num>0)
+{
+    console.log(num);
+    num--;
+}
---
+++ Q161.js
@@ -0,0 +1,12 @@
+function prasanna()
+{
+    console.log("how are you");
+}
+prasanna();
+
+let stay=function(num1 ,num2)
+{
+    console.log(num1+num2);
+    
+}
+stay(4,5,6);
